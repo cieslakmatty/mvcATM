b266c3f2849693484a5da7ce5f32f2fc27d77294
Mateusz Cieslak <cieslakmatty@gmail.com>
2017-10-01 14:01:44 +0100

    add Transfers functionality, update README, fix error checking

--- AutomatedTellerMachine/Controllers/TransactionController.cs
+++ AutomatedTellerMachine/Controllers/TransactionController.cs
@@ -47,22 +47,21 @@ namespace AutomatedTellerMachine.Controllers {
 
         [HttpPost]
         public ActionResult Withdraw(Transaction transaction) {
+            if (db.CheckingAccounts.Find(transaction.CheckingAccountId).Balance < Math.Abs(transaction.Amount)) {
+                //do they have enough money?
+                ModelState.AddModelError("Amount", "You've insufficient funds!");
+            }
             if (ModelState.IsValid) {
                 if (transaction.Amount >= 0) {
                     //make sure its always negative
                     transaction.Amount *= -1;
                 }
-                if (db.CheckingAccounts.Find(transaction.CheckingAccountId).Balance < Math.Abs(transaction.Amount)) {
-                    //do they have enough money?
-                    ViewBag.Error = "Funds too low";
-                    return View();
-                } else {
-                    db.Transactions.Add(transaction);
-                    db.SaveChanges();
-                    var service = new CheckingAccountService(db);
-                    service.UpdateBalance(transaction.CheckingAccountId);
-                    return RedirectToAction("Index", "Home");
-                }
+                db.Transactions.Add(transaction);
+                db.SaveChanges();
+                var service = new CheckingAccountService(db);
+                service.UpdateBalance(transaction.CheckingAccountId);
+                return RedirectToAction("Index", "Home");
+
             }
             return View();
         }
@@ -74,20 +73,83 @@ namespace AutomatedTellerMachine.Controllers {
         [HttpPost]
         public ActionResult QuickCash(Transaction transaction) {
             transaction.Amount = -100.00m;
+            if (db.CheckingAccounts.Find(transaction.CheckingAccountId).Balance < Math.Abs(transaction.Amount)) {
+                //do they have enough money?
+                ModelState.AddModelError("", "You've insufficient funds!");
+            }
             if (ModelState.IsValid) {
-                var test0 = db.CheckingAccounts.Find(transaction.CheckingAccountId).Balance;
-                var test1 = Math.Abs(transaction.Amount);
-                if (test0 < test1) {
-                    //do they have enough money?
-                    ViewBag.Error = "Funds too low";
-                    return View();
-                } else {
-                    db.Transactions.Add(transaction);
-                    db.SaveChanges();
-                    var service = new CheckingAccountService(db);
-                    service.UpdateBalance(transaction.CheckingAccountId);
-                    return RedirectToAction("Index", "Home");
+                db.Transactions.Add(transaction);
+                db.SaveChanges();
+                var service = new CheckingAccountService(db);
+                service.UpdateBalance(transaction.CheckingAccountId);
+                return RedirectToAction("Index", "Home");
+
+            }
+            return View();
+        }
+
+        public ActionResult Transfer(int checkingAccountId) {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Transfer(Transaction tempTransaction) {
+            //tempTransaction stores TO checkingAccountId
+
+            //get fromCheckingAccount
+            var UserId = User.Identity.GetUserId();
+            var fromCheckingAccount = db.CheckingAccounts.Where(c => c.ApplicationUserId == UserId).First();
+
+            if (db.CheckingAccounts.Find(fromCheckingAccount.Id).Balance < Math.Abs(tempTransaction.Amount)) {
+                //do they have enough money?
+                ModelState.AddModelError("Amount", "You've insufficient funds!");
+
+            }
+
+            if (db.CheckingAccounts.Find(tempTransaction.CheckingAccountId) == null) {
+                //does other account exist?
+                ModelState.AddModelError("CheckingaccountId", "That account does not exist!");
+            }
+
+            if (fromCheckingAccount.Id.Equals(tempTransaction.CheckingAccountId)) {
+                //cant send money to yourself
+                ModelState.AddModelError("CheckingaccountId", "You cannot send money to yourself!");
+            }
+            if (ModelState.IsValid) {
+                //get toCheckingAccount
+                var toCheckingAccount = db.CheckingAccounts.Find(tempTransaction.CheckingAccountId);
+
+                //create fromTransacation and toTransaction
+                var toTransaction = new Transaction {
+                    Amount = tempTransaction.Amount,
+                    CheckingAccountId = toCheckingAccount.Id,
+                    CheckingAccount = toCheckingAccount
+                };
+                var fromTransaction = new Transaction {
+                    Amount = tempTransaction.Amount,
+                    CheckingAccountId = fromCheckingAccount.Id,
+                    CheckingAccount = fromCheckingAccount
+                };
+
+                if (toTransaction.Amount <= 0) {
+                    //make sure TO is always positive
+                    toTransaction.Amount *= -1;
                 }
+                if (fromTransaction.Amount >= 0) {
+                    //make sure FROM is always negative
+                    fromTransaction.Amount *= -1;
+                }
+                //add to the other acc
+                db.Transactions.Add(toTransaction);
+                //add to curr acc
+                db.Transactions.Add(fromTransaction);
+                db.SaveChanges();
+                var service = new CheckingAccountService(db);
+                service.UpdateBalance(toTransaction.CheckingAccountId);
+                service.UpdateBalance(fromTransaction.CheckingAccountId);
+                return RedirectToAction("Index", "Home");
+
             }
             return View();
         }
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Tutorials for learning MVC
 
 ## Purpose
-The purpose of this proejct is to learn how to create and use an MVC project. After the tutials are completed I am aiming to create an mvc project that will use DOTA 2 website using opendota API to use as predictions platform.
+The purpose of this proejct is to learn how to create and use MVC. After the tutials are completed I am aiming to create an mvc project that will use DOTA 2 website using opendota API to use as predictions platform or complete another tutorial.
